3b36aa0077ea69128ab1b242c846d08fb2df1eb6
Rimantas <belauzas@yahoo.com>
2023-04-26 15:03:46 +0300

    Scroll

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,59 +1,61 @@
 // IMPORTS
 import { servicesData } from '../../data/servicesData.js';
+import { header } from '../components/header.js';
 import { services } from '../components/services.js';
 
 // FUNCTION EXECUTION
 
 /* header: start */
+header();
 /* header: end */
 
 /* hero: start */
 /* hero: end */
 
 /* about: start */
-const aboutTitleSpanDOM = document.getElementById('about');
-console.log(aboutTitleSpanDOM.innerText);
-console.log(aboutTitleSpanDOM.textContent);
+// const aboutTitleSpanDOM = document.getElementById('about');
+// console.log(aboutTitleSpanDOM.innerText);
+// console.log(aboutTitleSpanDOM.textContent);
 
-aboutTitleSpanDOM.textContent = 'me';
-console.log(aboutTitleSpanDOM.innerText);
-console.log(aboutTitleSpanDOM.textContent);
+// aboutTitleSpanDOM.textContent = 'me';
+// console.log(aboutTitleSpanDOM.innerText);
+// console.log(aboutTitleSpanDOM.textContent);
 
-const aboutSectionDOM = document.querySelector('.container.about');
-console.log(aboutSectionDOM);
+// const aboutSectionDOM = document.querySelector('.container.about');
+// console.log(aboutSectionDOM);
 
-const aboutTitleDOM = aboutSectionDOM.querySelector('.section-title');
-console.log(aboutTitleDOM.innerText);
+// const aboutTitleDOM = aboutSectionDOM.querySelector('.section-title');
+// console.log(aboutTitleDOM.innerText);
 
-const pDOM = aboutSectionDOM.querySelector('p');
-console.log(pDOM.classList);
+// const pDOM = aboutSectionDOM.querySelector('p');
+// console.log(pDOM.classList);
 
-pDOM.classList.add('btn');
-pDOM.classList.add('pomidoras');
-pDOM.classList.add('agurkas');
-pDOM.classList.remove('pomidoras');
-pDOM.classList.remove('btn');
-pDOM.classList.remove('agurkas');
+// pDOM.classList.add('btn');
+// pDOM.classList.add('pomidoras');
+// pDOM.classList.add('agurkas');
+// pDOM.classList.remove('pomidoras');
+// pDOM.classList.remove('btn');
+// pDOM.classList.remove('agurkas');
 
-pDOM.classList.add('labas', 'rytas', 'sakau', 'tau');
-pDOM.classList.remove('rytas', 'tau');
+// pDOM.classList.add('labas', 'rytas', 'sakau', 'tau');
+// pDOM.classList.remove('rytas', 'tau');
 
-pDOM.classList.toggle('sakau');
-pDOM.classList.toggle('sakau');
-pDOM.classList.toggle('sakau');
-pDOM.classList.toggle('sakau');
+// pDOM.classList.toggle('sakau');
+// pDOM.classList.toggle('sakau');
+// pDOM.classList.toggle('sakau');
+// pDOM.classList.toggle('sakau');
 
-if (pDOM.classList.contains('kamuolys')) {
-    pDOM.classList.remove('kamuolys');
-} else {
-    pDOM.classList.add('kamuolys');
-}
+// if (pDOM.classList.contains('kamuolys')) {
+//     pDOM.classList.remove('kamuolys');
+// } else {
+//     pDOM.classList.add('kamuolys');
+// }
 
-if (pDOM.classList.contains('kamuolys')) {
-    pDOM.classList.remove('kamuolys');
-} else {
-    pDOM.classList.add('kamuolys');
-}
+// if (pDOM.classList.contains('kamuolys')) {
+//     pDOM.classList.remove('kamuolys');
+// } else {
+//     pDOM.classList.add('kamuolys');
+// }
 
 /* about: end */
 
